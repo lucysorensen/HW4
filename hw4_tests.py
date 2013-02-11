f281fdb09d0a79a619cd0d56b731f03757cc83ad
lucysorensen <ls179@duke.edu>
2013-02-11 05:14:00 -0800

    Create hw4_tests.py

---
+++ hw4_tests.py
@@ -0,0 +1,18 @@
+import hw4
+
+newlist = hw4.LinkedList(4)
+print newlist
+newlist.addNode(8)
+print newlist
+newlist.addNode(6)
+print newlist
+newlist.addNodeAfter(12, 1)
+print newlist
+newlist.addNodeBefore(2, 1)
+print newlist
+newlist.removeNode(3)
+print newlist
+newlist.removeNodesbyValue(4)
+print newlist
+newlist.reverse()
+print newlist
